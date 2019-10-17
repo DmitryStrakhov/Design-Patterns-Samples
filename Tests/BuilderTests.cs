e80eb59907c60f38297b7ed10a25b196779a0e4a
Dmitry Strakhov <dmitry.strakhov.a@gmail.com>
2019-10-17 19:56:17 +0300

    #New GitHub Desktop - test commit

---
+++ Tests/BuilderTests.cs
@@ -0,0 +1,12 @@
+﻿#if DEBUG
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Design_Patterns_Samples.Tests {
+
+}
+#endif

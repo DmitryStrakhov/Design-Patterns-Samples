2c608e97c31a4ebbbbe4e55a1e5e8d671d15f476
Dmitry Strakhov <dmitry.strakhov.a@gmail.com>
2019-10-18 00:55:33 +0300

    #Builder

--- Creational/Builder.cs
+++ Creational/Builder.cs
@@ -6,4 +6,52 @@ using System.Threading.Tasks;
 
 namespace Design_Patterns_Samples {
 
+    public sealed class Director {
+        readonly ProductBuilderBase builder;
+
+        public Director(ProductBuilderBase builder) {
+            this.builder = builder;
+        }
+        public void Start() {
+            builder.BuildPart1();
+            builder.BuildPart2();
+            builder.BuildPart3();
+        }
+    }
+
+
+    public class ProductBuilderBase {
+        public virtual void BuildPart1() { }
+        public virtual void BuildPart2() { }
+        public virtual void BuildPart3() { }
+    }
+
+    public sealed class ProductABuilder : ProductBuilderBase {
+        ProductA product = null;
+
+        public override void BuildPart1() {
+            product = new ProductA();
+        }
+        public ProductA GetProduct() { return product; }
+    }
+
+    public sealed class ProductBBuilder : ProductBuilderBase {
+        ProductB product = null;
+
+        public override void BuildPart2() {
+            product = new ProductB();
+        }
+        public ProductB GetProduct() { return product; }
+    }
+
+
+    #region Builder Products
+
+    public sealed class ProductA {
+    }
+
+    public sealed class ProductB {
+    }
+
+    #endregion
 }
--- Tests/BuilderTests.cs
+++ Tests/BuilderTests.cs
@@ -5,8 +5,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Design_Patterns_Samples.Tests {
+    [TestClass]
+    public sealed class BuilderTests {
+        [TestMethod]
+        public void ProductABuilderTest() {
+            ProductABuilder builder = new ProductABuilder();
+            new Director(builder).Start();
+            ProductA product = builder.GetProduct();
+            AssertProduct(product, typeof(ProductA));
+        }
+        [TestMethod]
+        public void ProductBBuilderTest() {
+            ProductBBuilder builder = new ProductBBuilder();
+            new Director(builder).Start();
+            ProductB product = builder.GetProduct();
+            AssertProduct(product, typeof(ProductB));
+        }
 
+        private void AssertProduct(object product, Type expectedType) {
+            Assert.IsNotNull(product);
+            Assert.IsInstanceOfType(product, expectedType);
+        }
+    }
 }
 #endif

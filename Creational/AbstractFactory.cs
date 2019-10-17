bcc5307516d268546ad508d45106e28930adc426
Dmitry Strakhov <dmitry.strakhov.a@gmail.com>
2019-10-17 19:30:33 +0300

    #AbstractFactory

---
+++ Creational/AbstractFactory.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Design_Patterns_Samples {
+
+    public abstract class AbstractFactory {
+        public abstract AbstractProductA CreateProductA();
+        public abstract AbstractProductB CreateProductB();
+    }
+
+    public sealed class ConcreteFactory1 : AbstractFactory {
+        public override AbstractProductA CreateProductA() {
+            return new ProductA1();
+        }
+        public override AbstractProductB CreateProductB() {
+            return new ProductB1();
+        }
+    }
+
+    public sealed class ConcreteFactory2 : AbstractFactory {
+        public override AbstractProductA CreateProductA() {
+            return new ProductA2();
+        }
+        public override AbstractProductB CreateProductB() {
+            return new ProductB2();
+        }
+    }
+
+
+    #region Abstract Products
+
+    public abstract class AbstractProductA {
+    }
+
+    public abstract class AbstractProductB {
+    }
+
+    #endregion
+
+
+    #region Concrete Products
+
+    public sealed class ProductA1 : AbstractProductA {
+    }
+
+    public sealed class ProductA2 : AbstractProductA {
+    }
+
+    public sealed class ProductB1 : AbstractProductB {
+    }
+
+    public sealed class ProductB2 : AbstractProductB {
+    }
+
+    #endregion
+}
---
+++ Tests/AbstractFactoryTests.cs
@@ -0,0 +1,27 @@
+﻿#if DEBUG
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Design_Patterns_Samples.Tests {
+    [TestClass]
+    public sealed class AbstractFactoryTests {
+        [TestMethod]
+        public void ConcreteFactory1Test() {
+            AbstractFactory factory = new ConcreteFactory1();
+            Assert.IsInstanceOfType(factory.CreateProductA(), typeof(ProductA1));
+            Assert.IsInstanceOfType(factory.CreateProductB(), typeof(ProductB1));
+        }
+        [TestMethod]
+        public void ConcreteFactory2Test() {
+            AbstractFactory factory = new ConcreteFactory2();
+            Assert.IsInstanceOfType(factory.CreateProductA(), typeof(ProductA2));
+            Assert.IsInstanceOfType(factory.CreateProductB(), typeof(ProductB2));
+        }
+    }
+}
+#endif

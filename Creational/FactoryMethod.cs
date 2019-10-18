73f75c71478fb4bf939718ea14eb463e91820b1b
Dmitry Strakhov <dmitry.strakhov.a@gmail.com>
2019-10-18 19:18:30 +0300

    #FactoryMethod

---
+++ Creational/FactoryMethod.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Design_Patterns_Samples {
+
+    public abstract class AbstractCreator {
+        AbstractProduct product;
+
+        public AbstractCreator() {
+            this.product = null;
+        }
+
+        internal AbstractProduct Product {
+            get { return product ?? (product = CreateProduct()); }
+        }
+        protected abstract AbstractProduct CreateProduct();
+    }
+
+
+    public sealed class ConcreteCreator : AbstractCreator {
+        protected override AbstractProduct CreateProduct() {
+            return new ConcreteProduct();
+        }
+    }
+
+
+    #region Products
+
+    public abstract class AbstractProduct {
+    }
+
+    public sealed class ConcreteProduct : AbstractProduct {
+    }
+
+    #endregion
+}
---
+++ Tests/FactoryMethodTests.cs
@@ -0,0 +1,21 @@
+﻿#if DEBUG
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Design_Patterns_Samples.Tests {
+    [TestClass]
+    public class FactoryMethodTests {
+        [TestMethod]
+        public void ConcreteCreatorTest() {
+            ConcreteCreator creator = new ConcreteCreator();
+            Assert.IsNotNull(creator.Product);
+            Assert.IsInstanceOfType(creator.Product, typeof(ConcreteProduct));
+        }
+    }
+}
+#endif

0a368964f5365354caf306c122e03f888ee477ea
Dmitry Strakhov <dmitry.strakhov.a@gmail.com>
2019-10-21 00:43:28 +0300

    #Adapter

---
+++ Structural/Adapter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Design_Patterns_Samples {
+
+    public abstract class Target {
+        public abstract void Operation();
+    }
+
+    public sealed class Adapter : Target {
+        readonly Adaptee adaptee;
+
+        public Adapter(Adaptee adaptee) {
+            this.adaptee = adaptee;
+        }
+        public override void Operation() {
+            adaptee.SpecificOperation();
+        }
+    }
+
+
+    public sealed class Adaptee {
+        public Adaptee() {
+        }
+        public void SpecificOperation() {
+        }
+    }
+}

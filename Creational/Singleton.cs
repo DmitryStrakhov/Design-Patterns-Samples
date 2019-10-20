6b66bd3d37d4ef158ac2674391070e7c8f147e9c
Dmitry Strakhov <dmitry.strakhov.a@gmail.com>
2019-10-20 19:57:07 +0300

    #Singleton

---
+++ Creational/Singleton.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Design_Patterns_Samples {
+    public sealed class Singleton {
+        private Singleton() {
+        }
+        public void Start() {
+        }
+        public static readonly Singleton Instance = new Singleton();
+    }
+}
---
+++ Tests/SingletonTests.cs
@@ -0,0 +1,22 @@
+﻿#if DEBUG
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Design_Patterns_Samples.Tests {
+    [TestClass]
+    public sealed class SingletonTests {
+        [TestMethod]
+        public void SimpleTest() {
+            Singleton obj1 = Singleton.Instance;
+            Singleton obj2 = Singleton.Instance;
+            Assert.AreSame(obj1, obj2);
+            Singleton.Instance.Start();
+        }
+    }
+}
+#endif

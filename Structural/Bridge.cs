51f6b77775345cb5e16061d940c9d0b5bb606b20
Dmitry Strakhov <dmitry.strakhov.a@gmail.com>
2019-10-21 20:21:43 +0300

    #Bridge

---
+++ Structural/Bridge.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Design_Patterns_Samples {
+
+    public abstract class Abstraction {
+        readonly AbstractImplementor imp;
+
+        public Abstraction(AbstractImplementor imp) {
+            this.imp = imp;
+        }
+        public int Operation() {
+            return imp.OperationImp();
+        }
+    }
+
+    public sealed class RefinedAbstraction : Abstraction {
+        public RefinedAbstraction(AbstractImplementor imp)
+            : base(imp) {
+        }
+    }
+
+
+    public abstract class AbstractImplementor {
+        public abstract int OperationImp();
+    }
+
+    public sealed class ConcreteImplementorA : AbstractImplementor {
+        public override int OperationImp() { return 1; }
+    }
+
+    public sealed class ConcreteImplementorB : AbstractImplementor {
+        public override int OperationImp() { return 2; }
+    }
+}
---
+++ Tests/BridgeTests.cs
@@ -0,0 +1,28 @@
+﻿#if DEBUG
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Design_Patterns_Samples.Tests {
+    [TestClass]
+    public sealed class BridgeTests {
+        [TestMethod]
+        public void RefinedAbstractionATest() {
+            DoTest(new ConcreteImplementorA(), 1);
+        }
+        [TestMethod]
+        public void RefinedAbstractionBTest() {
+            DoTest(new ConcreteImplementorB(), 2);
+        }
+
+        private void DoTest(AbstractImplementor imp, int expectedResult) {
+            RefinedAbstraction obj = new RefinedAbstraction(imp);
+            Assert.AreEqual(expectedResult, obj.Operation());
+        }
+    }
+}
+#endif

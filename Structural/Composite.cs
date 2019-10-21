ad6f8fa25ffcd26df03d7a8988a6102e734250c6
Dmitry Strakhov <dmitry.strakhov.a@gmail.com>
2019-10-22 00:53:30 +0300

    #Composite

---
+++ Structural/Composite.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Design_Patterns_Samples {
+
+    public abstract class Component {
+        public virtual void Operation() {
+        }
+
+        public virtual void Add(Component component) {
+            throw new NotImplementedException();
+        }
+        public virtual void Remove(Component component) {
+            throw new NotImplementedException();
+        }
+    }
+
+
+    public sealed class Leaf : Component {
+        public override void Operation() {
+        }
+    }
+
+
+    public sealed class Composite : Component {
+        public override void Operation() {
+        }
+        public override void Add(Component component) {
+            // code
+        }
+        public override void Remove(Component component) {
+            // code
+        }
+    }
+}
---
+++ Tests/CompositeTests.cs
@@ -0,0 +1,25 @@
+﻿#if DEBUG
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Design_Patterns_Samples.Tests {
+    [TestClass]
+    public sealed class CompositeTests {
+        [TestMethod]
+        public void SimpleTest() {
+            Composite composite = new Composite();
+            composite.Add(new Leaf());
+            composite.Add(new Leaf());
+
+            Composite childComposite = new Composite();
+            composite.Add(childComposite);
+            childComposite.Add(new Leaf());
+        }
+    }
+}
+#endif

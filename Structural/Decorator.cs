ce0a8d84594f4dd0d53128b7719f56cc98d5c665
Dmitry Strakhov <dmitry.strakhov.a@gmail.com>
2019-10-22 19:29:35 +0300

    #Decorator

---
+++ Structural/Decorator.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Design_Patterns_Samples {
+
+    public abstract class BaseComponent {
+        public abstract void Operation();
+    }
+
+    public sealed class ConcreteComponent : BaseComponent {
+        public override void Operation() {
+        }
+    }
+
+
+    public abstract class Decorator : BaseComponent {
+        readonly BaseComponent component;
+
+        public Decorator(BaseComponent component) {
+            this.component = component;
+        }
+        public override void Operation() {
+            component.Operation();
+        }
+    }
+
+    public sealed class ConcreteDecoratorA : Decorator {
+        public ConcreteDecoratorA(BaseComponent component)
+            : base(component) {
+        }
+        public override void Operation() {
+            base.Operation();
+        }
+    }
+
+    public sealed class ConcreteDecoratorB : Decorator {
+        public ConcreteDecoratorB(BaseComponent component)
+            : base(component) {
+        }
+        public override void Operation() {
+            base.Operation();
+        }
+    }
+}
---
+++ Tests/DecoratorTests.cs
@@ -0,0 +1,24 @@
+﻿#if DEBUG
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Design_Patterns_Samples.Tests {
+    [TestClass]
+    public sealed class DecoratorTests {
+        [TestMethod]
+        public void SimpleTest() {
+            BaseComponent component = new ConcreteComponent();
+            DoOperation(new ConcreteDecoratorA(new ConcreteDecoratorB(component)));
+        }
+
+        private void DoOperation(BaseComponent component) {
+            component.Operation();
+        }
+    }
+}
+#endif

057a5f9b1ff2d7a66138b3c38dee404b57d1746e
Dmitry Strakhov <dmitry.strakhov.a@gmail.com>
2019-10-24 00:34:02 +0300

    #Flyweight

---
+++ Structural/Flyweight.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Design_Patterns_Samples {
+
+    public abstract class Flyweight {
+        public abstract void Operation(ExtrinsicState extrinsicState);
+    }
+
+    public sealed class SharedConcreteFlyweight : Flyweight {
+        readonly IntrinsicState intrinsicState;
+
+        public SharedConcreteFlyweight() {
+            this.intrinsicState = new IntrinsicState();
+        }
+        public override void Operation(ExtrinsicState extrinsicState) {
+        }
+    }
+
+    public sealed class NonsharedConcreteFlyweight : Flyweight {
+        readonly FullState fullState;
+
+        public NonsharedConcreteFlyweight() {
+            this.fullState = new FullState();
+        }
+        public override void Operation(ExtrinsicState extrinsicState) {
+        }
+    }
+
+
+    #region Factory
+
+    public abstract class FlyweightFactory {
+        public abstract Flyweight GetFlyweight(char code);
+    }
+
+    public sealed class DefaultFlyweightFactory : FlyweightFactory {
+        readonly FlyweightCache cache;
+
+        public DefaultFlyweightFactory() {
+            this.cache = new FlyweightCache();
+        }
+        public override Flyweight GetFlyweight(char code) {
+            return cache.GetOrAdd(code);
+        }
+    }
+
+    sealed class FlyweightCache : Dictionary<char, Flyweight> {
+        public Flyweight GetOrAdd(char code) {
+            Flyweight flyweight;
+
+            if(!TryGetValue(code, out flyweight)) {
+                this[code] = flyweight = new SharedConcreteFlyweight();
+            }
+            return flyweight;
+        }
+    }
+
+    #endregion
+
+
+    #region States
+
+    public sealed class IntrinsicState {
+    }
+
+    public sealed class ExtrinsicState {
+    }
+
+    public sealed class FullState {
+    }
+
+    #endregion
+}
---
+++ Tests/FlyweightTests.cs
@@ -0,0 +1,31 @@
+﻿#if DEBUG
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Design_Patterns_Samples.Tests {
+    [TestClass]
+    public sealed class FlyweightTests {
+        [TestMethod]
+        public void SimpleTest1() {
+            DefaultFlyweightFactory factory = new DefaultFlyweightFactory();
+            Flyweight obj1 = factory.GetFlyweight('x');
+            Flyweight obj2 = factory.GetFlyweight('x');
+            Flyweight obj3 = factory.GetFlyweight('x');
+            Assert.AreSame(obj1, obj2);
+            Assert.AreSame(obj2, obj3);
+        }
+        [TestMethod]
+        public void SimpleTest2() {
+            DefaultFlyweightFactory factory = new DefaultFlyweightFactory();
+            Flyweight obj1 = factory.GetFlyweight('a');
+            Flyweight obj2 = factory.GetFlyweight('b');
+            Assert.AreNotSame(obj1, obj2);
+        }
+    }
+}
+#endif

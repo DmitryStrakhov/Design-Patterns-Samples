c0a650415812bca9f5824aebe2b02b3a016dd01b
Dmitry Strakhov <dmitry.strakhov.a@gmail.com>
2019-10-24 19:41:57 +0300

    #Proxy

---
+++ Structural/Proxy.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Design_Patterns_Samples {
+
+    public abstract class Subject {
+        public abstract Data OperationA();
+        public abstract void OperationB();
+    }
+
+    public sealed class RealSubject : Subject {
+        public override Data OperationA() {
+            return new Data();
+        }
+        public override void OperationB() {
+        }
+    }
+
+    public sealed class Proxy : Subject {
+        readonly Data data;
+        RealSubject realSubject;
+
+        public Proxy(Data data) {
+            this.data = data;
+            this.realSubject = null;
+        }
+
+        public override Data OperationA() {
+            if(realSubject != null)
+                return realSubject.OperationA();
+            return data;
+        }
+        public override void OperationB() {
+            if(realSubject == null)
+                realSubject = new RealSubject();
+            realSubject.OperationB();
+        }
+    }
+
+
+    public sealed class Data {
+    }
+}
---
+++ Tests/ProxyTests.cs
@@ -0,0 +1,33 @@
+﻿#if DEBUG
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Design_Patterns_Samples.Tests {
+    [TestClass]
+    public sealed class ProxyTests {
+        [TestMethod]
+        public void SimpleTest() {
+            Data proxyData = new Data();
+            Proxy proxy = new Proxy(proxyData);
+
+            Data resultData = DoOperationA(proxy);
+            Assert.AreEqual(proxyData, resultData);
+            DoOperationB(proxy);
+            resultData = DoOperationA(proxy);
+            Assert.AreNotEqual(proxyData, resultData);
+        }
+
+        private Data DoOperationA(Subject subject) {
+            return subject.OperationA();
+        }
+        private void DoOperationB(Subject subject) {
+            subject.OperationB();
+        }
+    }
+}
+#endif

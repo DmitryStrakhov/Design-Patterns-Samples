d01d6e0b97735d38b3ce76477dfe4f0a5db8ed4b
Dmitry Strakhov <dmitry.strakhov.a@gmail.com>
2019-10-25 19:29:52 +0300

    #Command

---
+++ Behavioral/Command.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Design_Patterns_Samples {
+
+    public sealed class Invoker {
+        Command command;
+
+        public Invoker() {
+            this.command = null;
+        }
+        public void SetCommand(Command command) {
+            this.command = command;
+        }
+        public void Run() {
+            command?.Execute();
+        }
+    }
+
+
+    public abstract class Command {
+        public abstract void Execute();
+    }
+
+    public sealed class ConcreteCommand : Command {
+        readonly Receiver receiver;
+
+        public ConcreteCommand(Receiver receiver) {
+            this.receiver = receiver;
+        }
+        public override void Execute() {
+            receiver.Action1();
+            receiver.Action2();
+        }
+    }
+
+
+    public sealed class Receiver {
+        public void Action1() { }
+        public void Action2() { }
+    }
+}
---
+++ Tests/CommandTests.cs
@@ -0,0 +1,26 @@
+﻿#if DEBUG
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Design_Patterns_Samples.Tests {
+    [TestClass]
+    public sealed class CommandTests {
+        [TestMethod]
+        public void SimpleTest() {
+            Command command = BuildCommand();
+            Invoker invoker = new Invoker();
+            invoker.SetCommand(command);
+            invoker.Run();
+        }
+
+        private Command BuildCommand() {
+            return new ConcreteCommand(new Receiver());
+        }
+    }
+}
+#endif

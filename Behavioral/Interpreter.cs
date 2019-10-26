d1038f7bcb7bdd3248fd829110075363f0ee6a97
Dmitry Strakhov <dmitry.strakhov.a@gmail.com>
2019-10-27 00:43:51 +0300

    #Interpreter

---
+++ Behavioral/Interpreter.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Design_Patterns_Samples {
+
+    public abstract class BooleanExpression {
+        public abstract bool Evaluate(EvaluationContext context);
+    }
+
+    public sealed class EvaluationContext {
+        readonly Dictionary<string, bool> values;
+
+        public EvaluationContext() {
+            this.values = new Dictionary<string, bool>();
+        }
+        public void AddVariable(string name, bool value) {
+            values[name] = value;
+        }
+        public bool GetVariableValue(string name) => values[name];
+    }
+
+
+    public sealed class AndExpression : BooleanExpression {
+        readonly BooleanExpression lExp;
+        readonly BooleanExpression rExp;
+
+        public AndExpression(BooleanExpression lExp, BooleanExpression rExp) {
+            this.lExp = lExp;
+            this.rExp = rExp;
+        }
+        public override bool Evaluate(EvaluationContext context) {
+            return lExp.Evaluate(context) && rExp.Evaluate(context);
+        }
+    }
+
+    public sealed class OrExpression : BooleanExpression {
+        readonly BooleanExpression lExp;
+        readonly BooleanExpression rExp;
+
+        public OrExpression(BooleanExpression lExp, BooleanExpression rExp) {
+            this.lExp = lExp;
+            this.rExp = rExp;
+        }
+        public override bool Evaluate(EvaluationContext context) {
+            return lExp.Evaluate(context) || rExp.Evaluate(context);
+        }
+    }
+
+    public sealed class NotExpression : BooleanExpression {
+        readonly BooleanExpression exp;
+
+        public NotExpression(BooleanExpression exp) {
+            this.exp = exp;
+        }
+        public override bool Evaluate(EvaluationContext context) {
+            return !exp.Evaluate(context);
+        }
+    }
+
+    public sealed class VariableExpression : BooleanExpression {
+        readonly string name;
+
+        public VariableExpression(string name) {
+            this.name = name;
+        }
+        public override bool Evaluate(EvaluationContext context) {
+            return context.GetVariableValue(name);
+        }
+    }
+
+    public sealed class ConstantExpression : BooleanExpression {
+        readonly bool value;
+
+        public ConstantExpression(bool value) {
+            this.value = value;
+        }
+        public override bool Evaluate(EvaluationContext context) => value;
+    }
+}
---
+++ Tests/InterpreterTests.cs
@@ -0,0 +1,43 @@
+﻿#if DEBUG
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Design_Patterns_Samples.Tests {
+    [TestClass]
+    public sealed class InterpreterTests {
+        [TestMethod]
+        public void Test1() {
+            EvaluationContext context = new EvaluationContext();
+            context.AddVariable("X", true);
+            context.AddVariable("Y", false);
+
+            BooleanExpression expression = BuildExpression();
+            Assert.IsTrue(expression.Evaluate(context));
+        }
+        [TestMethod]
+        public void Test2() {
+            EvaluationContext context = new EvaluationContext();
+            context.AddVariable("X", false);
+            context.AddVariable("Y", false);
+
+            BooleanExpression expression = BuildExpression();
+            Assert.IsFalse(expression.Evaluate(context));
+        }
+
+        private BooleanExpression BuildExpression() {
+            VariableExpression xExp = new VariableExpression("X");
+            VariableExpression yExp = new VariableExpression("Y");
+
+            return new OrExpression(
+                new AndExpression(new ConstantExpression(true), xExp),
+                new AndExpression(yExp, new NotExpression(xExp))
+            );
+        }
+    }
+}
+#endif

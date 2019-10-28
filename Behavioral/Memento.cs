edb68783c812635fc1c246bbba9a034cb9c67fba
Dmitry Strakhov <dmitry.strakhov.a@gmail.com>
2019-10-29 00:55:42 +0300

    #Memento

---
+++ Behavioral/Memento.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Design_Patterns_Samples {
+
+    public interface IMemento {
+    }
+
+
+    public sealed class Originator {
+        InternalState state;
+
+        public Originator() {
+            this.state = new InternalState();
+        }
+        public void Action() {
+            // change state somehow
+        }
+        public IMemento CreateMemento() {
+            return new Memento(state);
+        }
+        public void SetMemento(IMemento memento) {
+            state = ((Memento)memento).GetState();
+        }
+
+        #region Memento
+
+        private sealed class Memento : IMemento {
+            readonly InternalState state;
+
+            public Memento(InternalState state) {
+                this.state = state;
+            }
+            public InternalState GetState() => state;
+        }
+
+        #endregion
+    }
+
+
+    sealed class InternalState {
+    }
+}
---
+++ Tests/MementoTests.cs
@@ -0,0 +1,23 @@
+﻿#if DEBUG
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Design_Patterns_Samples.Tests {
+    [TestClass]
+    public sealed class MementoTests {
+        [TestMethod]
+        public void SimpleTest() {
+            Originator obj = new Originator();
+
+            IMemento memento = obj.CreateMemento();
+            obj.Action();
+            obj.SetMemento(memento);
+        }
+    }
+}
+#endif

8168cfb675028b6d78c127f8647593d10b54ba9c
Dmitry Strakhov <dmitry.strakhov.a@gmail.com>
2019-10-30 00:35:21 +0300

    #State

---
+++ Behavioral/State.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Design_Patterns_Samples {
+
+    public sealed class Context {
+        StateBase currentState;
+
+        public Context() {
+            this.currentState = new State1();
+        }
+        public void Action() {
+            currentState.Action(this);
+        }
+        private void ChangeState(StateBase state) {
+            this.currentState = state;
+        }
+
+        #region Context States
+
+        abstract class StateBase {
+            public abstract void Action(Context context);
+        }
+
+        sealed class State1 : StateBase {
+            public override void Action(Context context) {
+                // do action
+                context.ChangeState(new State2());
+            }
+        }
+
+        sealed class State2 : StateBase {
+            public override void Action(Context context) {
+                // do action
+                context.ChangeState(new State3());
+            }
+        }
+
+        sealed class State3 : StateBase {
+            public override void Action(Context context) {
+                // do action
+                context.ChangeState(new State1());
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ Tests/StateTests.cs
@@ -0,0 +1,22 @@
+﻿#if DEBUG
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Design_Patterns_Samples.Tests {
+    [TestClass]
+    public sealed class StateTests {
+        [TestMethod]
+        public void SimpleTest() {
+            Context context = new Context();
+            context.Action();
+            context.Action();
+            context.Action();
+        }
+    }
+}
+#endif

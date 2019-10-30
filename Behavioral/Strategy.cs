ca2c0f38a8545164c7d074df9018b56bc51be1b3
Dmitry Strakhov <dmitry.strakhov.a@gmail.com>
2019-10-30 21:03:51 +0300

    #Strategy

---
+++ Behavioral/Strategy.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Design_Patterns_Samples {
+
+    public sealed class ContextObj {
+        readonly StrategyBase strategy;
+
+        public ContextObj(StrategyBase strategy) {
+            this.strategy = strategy;
+        }
+        public void Request() => strategy.Action();
+    }
+
+    public abstract class StrategyBase {
+        public abstract void Action();
+    }
+
+
+    public sealed class ConcreteStrategyA : StrategyBase {
+        public override void Action() {
+            // do action
+        }
+    }
+
+    public sealed class ConcreteStrategyB : StrategyBase {
+        public override void Action() {
+            // do action
+        }
+    }
+
+    public sealed class ConcreteStrategyC : StrategyBase {
+        public override void Action() {
+            // do action
+        }
+    }
+}
---
+++ Tests/StrategyTests.cs
@@ -0,0 +1,32 @@
+﻿#if DEBUG
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Design_Patterns_Samples.Tests {
+    [TestClass]
+    public sealed class StrategyTests {
+        [TestMethod]
+        public void ConcreteStrategyATest() {
+            DoTest(new ConcreteStrategyA());
+        }
+        [TestMethod]
+        public void ConcreteStrategyBTest() {
+            DoTest(new ConcreteStrategyB());
+        }
+        [TestMethod]
+        public void ConcreteStrategyCTest() {
+            DoTest(new ConcreteStrategyC());
+        }
+
+        private void DoTest(StrategyBase strategy) {
+            ContextObj context = new ContextObj(strategy);
+            context.Request();
+        }
+    }
+}
+#endif

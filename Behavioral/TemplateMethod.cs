c25998b5d4095197171aab445635f2a69e3e94bc
Dmitry Strakhov <dmitry.strakhov.a@gmail.com>
2019-10-31 01:04:11 +0300

    #Template Method

---
+++ Behavioral/TemplateMethod.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Design_Patterns_Samples {
+
+    public abstract class AbstractClass {
+        public void TemplateMethod() {
+            int value = GetValue();
+            PrimitiveOperation(value);
+            SetValue(value);
+            HookOperation(value);
+        }
+
+        #region Concrete Operations
+
+        private int GetValue() {
+            return 0;
+        }
+        private void SetValue(int value) {
+            // do something
+        }
+
+        #endregion
+
+        #region Hooks
+
+        protected virtual void HookOperation(int value) {
+        }
+
+        #endregion
+
+        protected abstract void PrimitiveOperation(int value);
+    }
+
+    public sealed class ConcreteClass : AbstractClass {
+        protected override void PrimitiveOperation(int value) {
+            // do something
+        }
+        protected override void HookOperation(int value) {
+            // hook here
+        }
+    }
+}
---
+++ Tests/TemplateMethodTests.cs
@@ -0,0 +1,20 @@
+﻿#if DEBUG
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Design_Patterns_Samples.Tests {
+    [TestClass]
+    public sealed class TemplateMethodTests {
+        [TestMethod]
+        public void SimpleTest() {
+            ConcreteClass concreteClass = new ConcreteClass();
+            concreteClass.TemplateMethod();
+        }
+    }
+}
+#endif

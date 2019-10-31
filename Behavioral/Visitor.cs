2b7dfe3bbe30cae653da292c916a8d934ed85cce
Dmitry Strakhov <dmitry.strakhov.a@gmail.com>
2019-10-31 20:38:47 +0300

    #Visitor

---
+++ Behavioral/Visitor.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Design_Patterns_Samples {
+
+    public abstract class Visitor {
+        public abstract void VisitConcreteElementA(ConcreteElementA element);
+        public abstract void VisitConcreteElementB(ConcreteElementB element);
+        public abstract void VisitConcreteElementC(ConcreteElementC element);
+    }
+
+    public abstract class Element {
+        public abstract void Accept(Visitor visitor);
+    }
+
+
+    #region Concrete Elements
+
+    public sealed class ConcreteElementA : Element {
+        public override void Accept(Visitor visitor) {
+            visitor.VisitConcreteElementA(this);
+        }
+    }
+
+    public sealed class ConcreteElementB : Element {
+        public override void Accept(Visitor visitor) {
+            visitor.VisitConcreteElementB(this);
+        }
+    }
+
+    public sealed class ConcreteElementC : Element {
+        readonly List<Element> childList;
+
+        public ConcreteElementC() {
+            this.childList = new List<Element>();
+        }
+        public ConcreteElementC AddElement(Element element) {
+            childList.Add(element);
+            return this;
+        }
+        public override void Accept(Visitor visitor) {
+            visitor.VisitConcreteElementC(this);
+            childList.ForEach(x => x.Accept(visitor));
+        }
+    }
+
+    #endregion
+
+    #region Concrete Visitors
+
+    public sealed class ConcreteVisitorA : Visitor {
+        int visitCount;
+
+        public ConcreteVisitorA() {
+            this.visitCount = 0;
+        }
+        public override void VisitConcreteElementA(ConcreteElementA element) => visitCount++;
+        public override void VisitConcreteElementB(ConcreteElementB element) => visitCount++;
+        public override void VisitConcreteElementC(ConcreteElementC element) => visitCount++;
+        public int VisitCount { get { return visitCount; } }
+    }
+
+    public sealed class ConcreteVisitorB : Visitor {
+        readonly StringBuilder stringBuilder;
+
+        public ConcreteVisitorB() {
+            this.stringBuilder = new StringBuilder();
+        }
+
+        public override void VisitConcreteElementA(ConcreteElementA element) {
+            stringBuilder.Append("ElementA;");
+        }
+        public override void VisitConcreteElementB(ConcreteElementB element) {
+            stringBuilder.Append("ElementB;");
+        }
+        public override void VisitConcreteElementC(ConcreteElementC element) {
+            stringBuilder.Append("ElementC;");
+        }
+        public string GetTrace() { return stringBuilder.ToString(); }
+    }
+
+    #endregion
+}
---
+++ Tests/VisitorTests.cs
@@ -0,0 +1,41 @@
+﻿#if DEBUG
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Design_Patterns_Samples.Tests {
+    [TestClass]
+    public sealed class VisitorTests {
+        [TestMethod]
+        public void ConcreteVisitorATest() {
+            ConcreteVisitorA visitor = new ConcreteVisitorA();
+            Element composition = BuildComposition();
+            composition.Accept(visitor);
+            Assert.AreEqual(5, visitor.VisitCount);
+        }
+        [TestMethod]
+        public void ConcreteVisitorBTest() {
+            ConcreteVisitorB visitor = new ConcreteVisitorB();
+            Element composition = BuildComposition();
+            composition.Accept(visitor);
+            Assert.AreEqual("ElementC;ElementA;ElementC;ElementA;ElementB;", visitor.GetTrace());
+        }
+
+        private Element BuildComposition() {
+            ConcreteElementA a1 = new ConcreteElementA();
+            ConcreteElementA a2 = new ConcreteElementA();
+            ConcreteElementB b1 = new ConcreteElementB();
+            ConcreteElementC c1 = new ConcreteElementC();
+            ConcreteElementC c2 = new ConcreteElementC();
+
+            c1.AddElement(a1).AddElement(c2);
+            c2.AddElement(a2).AddElement(b1);
+            return c1;
+        }
+    }
+}
+#endif
